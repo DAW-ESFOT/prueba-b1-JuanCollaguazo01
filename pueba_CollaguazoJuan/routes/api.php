e0919fbe03c19088e71353f304fc9a096dd04a1b
JuanCollaguazo01 <38759831+JuanCollaguazo01@users.noreply.github.com>
2021-01-06 21:58:20 -0500

    gen y user

--- pueba_CollaguazoJuan/routes/api.php
+++ pueba_CollaguazoJuan/routes/api.php
@@ -20,3 +20,16 @@ Route::get('movies/{movie}', 'MovieController@show');
 Route::post('movies', 'MovieController@store');
 Route::put('movies/{movie}', 'MovieController@update');
 Route::delete('movies/{movie}', 'MovieController@delete');
+
+
+Route::get('genres', 'GenreController@index');
+Route::get('genres/{genre}', 'GenreController@show');
+Route::post('genres', 'GenreController@store');
+Route::put('genres/{genre}', 'GenreController@update');
+Route::delete('genres/{genre}', 'GenreController@delete');
+
+Route::get('users', 'UserController@index');
+Route::get('users/{user}', 'UserController@show');
+Route::post('users', 'UserController@store');
+Route::put('users/{user}', 'UserController@update');
+Route::delete('users/{user}', 'UserController@delete');
